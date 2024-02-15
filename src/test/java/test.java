9f91ed818961c02a70bfce9b6abd65cafa99731e
saredk <sare.sogut@gmail.com>
2024-02-15 21:07:48 +0000

    the first commit

---
+++ src/test/java/test.java
@@ -0,0 +1,6 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("i am batch18 student");
+        System.out.println("new commit by new user");
+    }
+}
